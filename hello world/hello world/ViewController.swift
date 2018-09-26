ad4a0b2f8ef1ebfb23bf3d6adc82610567692556
Arik Rundquist <admin@Ariks-iMac.local>
2018-09-25 19:50:35 -0700

    Hello world

---
+++ hello world/hello world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by Arik Rundquist on 9/25/18.
+//  Copyright © 2018 Arik Rundquist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
